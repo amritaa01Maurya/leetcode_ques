9f5d7a05ab6a124f813f0dd6032d4cca9fbc3284
Amrita Maurya <mauryaamrita835@gmail.com>
2025-10-02 02:00:58 +0530

    Time: 2 ms (93.17%), Space: 55.6 MB (87.65%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int n = bills.length;
+        int totChange = 0;
+        int b_5 = 0;
+        int b_10 = 0;
+        int b_20 = 0;
+        for(int i=0;i<n;i++){
+            if(bills[i] == 20){
+                b_20++;
+                if(b_10 > 0 && b_5 > 0){
+                    b_10--;
+                    b_5--;
+                }else if(b_5 >= 3){
+                    b_5 -= 3;
+                }else{
+                    return false;
+                }
+            }
+            else if(bills[i] == 10){
+                b_10++;
+                if(b_5 > 0){
+                    b_5--;
+                }else{
+                    return false;
+                }
+            }else{
+                b_5++;
+            }
+        }
+        return true;
+    }
+}
