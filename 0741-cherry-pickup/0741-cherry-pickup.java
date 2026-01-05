504e423c45fc68a100aa28b054d7750c801b6e39
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-05 21:29:54 +0530

    Time: 15 ms (61.59%), Space: 47.1 MB (62.78%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,46 +1,46 @@
 class Solution {
-    public int dfs(int[][] grid, int[][][][] dp , int i, int j, int k, int l) {
+    public int dfs(int[][] grid, int[][][] dp, int i, int j, int k) {
         int n = grid.length;
-        int m = grid[0].length;
-        if(i >= n || j >= m || k >= n || l >= m || grid[i][j] == -1 || grid[k][l] == -1){
+        int l = i + j - k;
+
+        if (i >= n || j >= n || k >= n || l >= n || grid[i][j] == -1 || grid[k][l] == -1) {
             return Integer.MIN_VALUE;
         }
-        if(i == n - 1 && j == m - 1){
+        if (i == n - 1 && j == n - 1) {
             return grid[i][j];
         }
 
-        if(dp[i][j][k][l] != -1){
-            return dp[i][j][k][l];
+        if (dp[i][j][k] != -1) {
+            return dp[i][j][k];
         }
 
         int ans = grid[i][j];
-        if(i != k || j != l){
+        if (i != k || j != l) {
             ans += grid[k][l];
         }
 
-        int RR = dfs(grid, dp, i, j + 1, k, l + 1);
-        int DR = dfs(grid, dp, i + 1, j, k, l + 1);
+        int RR = dfs(grid, dp, i, j + 1, k);
+        int DR = dfs(grid, dp, i + 1, j, k);
 
-        int DD = dfs(grid, dp, i + 1 , j ,k + 1, l);
-        int RD = dfs(grid, dp, i, j + 1,k + 1, l);
+        int DD = dfs(grid, dp, i + 1, j, k + 1);
+        int RD = dfs(grid, dp, i, j + 1, k + 1);
 
         int maxPick = Math.max(RR, Math.max(DR, Math.max(DD, RD)));
-        
-        return dp[i][j][k][l] = ans + maxPick;
+
+        return dp[i][j][k] = ans + maxPick;
     }
+
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
-        int[][][][] dppp = new int[n][m][n][m];
-        for(int[][][] dpp:dppp){
-            for(int[][] dp:dpp){
-                for(int[] d:dp){
-                    Arrays.fill(d, -1);
-                }
+
+        int[][][] dppp = new int[n][n][n];
+        for (int[][] dpp : dppp) {
+            for (int[] dp : dpp) {
+                Arrays.fill(dp, -1);
             }
         }
 
-        return Math.max(0,dfs(grid, dppp, 0, 0, 0, 0));
+        return Math.max(0, dfs(grid, dppp, 0, 0, 0));
 
     }
 }
