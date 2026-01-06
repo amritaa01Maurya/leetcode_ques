92a94b708e3802418ff3af3ad7c18f9f30338d40
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-06 23:39:25 +0530

    Time: 5 ms (48.35%), Space: 46.5 MB (27.12%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        Arrays.sort(trips, new Comparator<int[]>(){ 
-            @Override 
-            public int compare(int[] p1, int[] p2){ 
-                if(p1[1] == p2[1]){ 
-                    return Integer.compare(p1[2], p2[2]); 
-                } 
-                return Integer.compare(p1[1], p2[1]); 
-            } 
-        });
-
         int[] prefix = new int[1001];
 
         for(int i=0;i<trips.length; i++){
@@ -20,8 +10,7 @@ class Solution {
                 if(prefix[j] > capacity){
                     return false;
                 }
-            }
-            
+            }  
         }
         return true;
     }
