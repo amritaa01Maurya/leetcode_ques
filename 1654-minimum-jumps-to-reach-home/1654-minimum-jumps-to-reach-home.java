a3d076a0c4052733b045a3f491dfa32c032ccfed
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-06 15:41:03 +0530

    Time: 39 ms (27.42%), Space: 47.6 MB (28.23%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
@@ -10,18 +10,22 @@ class Solution {
         }
     }
     public int minimumJumps(int[] forbidden, int a, int b, int x) {
-        HashMap<String, Integer> map = new HashMap<>();
+
         Set<Integer> set = new HashSet<>();
         int maxFB = 0;
         for(int f:forbidden){
             set.add(f);
             maxFB = Math.max(f, maxFB);
         }
-        map.put(0+","+"#", 0);
+        
+        int limit = 6000;
+        boolean[][] vis = new boolean[limit+1][2];
+
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(0, '#', 0)); // pos, prev, step
 
-        int limit = 6000;
+        vis[0][0] =true;
+        vis[0][1]=true;
 
         while(!q.isEmpty()){
             Pair p = q.remove();
@@ -37,15 +41,14 @@ class Solution {
             if(prvMove != 'b' && nd - b >= 0){
                 back = nd - b;
                 String key = back+","+"b";
-                if(!set.contains(back) && map.getOrDefault(key, Integer.MAX_VALUE) > st + 1){
-                    map.put(key, st + 1);
+                if(!set.contains(back) && !vis[back][1]){
+                    vis[back][1] = true;;
                     q.add(new Pair(back, 'b', st + 1));
                 }
             }
             int forw = nd + a;
-            String k = forw + "," + "a";
-            if(forw < limit && !set.contains(forw) && map.getOrDefault(k, Integer.MAX_VALUE) > st + 1){
-                map.put(k, st + 1);
+            if(forw < limit && !set.contains(forw) &&  !vis[forw][0]){
+                vis[forw][0]=true;;
                 q.add(new Pair(forw, 'a', st + 1));
             }
         }
