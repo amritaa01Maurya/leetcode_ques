61389659c600ba825cddf4e31c84b6f760e27ec1
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-15 01:39:51 +0530

    Time: 0 ms (100%), Space: 42.5 MB (30.21%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -1,40 +1,13 @@
 class Solution {
-    public int solve(List<Integer> list, int i, int num, int l, int[][] dp) {
-        if(i >= list.size() || num < 0){
-            return Integer.MAX_VALUE;
-        }
-        if(num == 0){
-            return l;
-        }
-        if(dp[i][num] != -1){
-            return dp[i][num];
-        }
-        int inc1 = solve(list, i, num - list.get(i), l + 1, dp);
-        int inc2 = solve(list, i + 1, num - list.get(i), l + 1, dp);
-        int exc = solve(list, i+1, num, l, dp);
-
-        return dp[i][num] = Math.min(Math.min(inc1, inc2), exc);
-    }
     public int minimumNumbers(int num, int k) {
         if(num == 0){
             return 0;
         }
-        if(k == 0){
-            return num % 10 == 0 ? 1: -1;
+        for(int i=1;i<11;i++){
+            if((i * k) <= num && (i * k) % 10 == num % 10){
+                return i;
+            }
         }
-        List<Integer> l = new ArrayList<>();
-        int val = k;
-        l.add(val);
-        while(val <= num){
-            val += 10;
-            l.add(val);
-        }
-        int[][] dp = new int[l.size()][num+1];
-        for(int[] d:dp){
-            Arrays.fill(d, -1);
-        }
-
-        int ans = solve(l, 0, num, 0, dp);
-        return ans == Integer.MAX_VALUE ?  -1: ans; 
+        return -1;
     }
 }
