07ec8aaa27255448e25677dbb14ec0fbc16a8f5a
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-17 22:35:02 +0530

    Time: 71 ms (88.09%), Space: 201.4 MB (78.06%) - LeetHub

--- 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.java
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.java
@@ -4,41 +4,44 @@ class Solution {
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
+
         for(int[] e:edges){
             int u = e[0];
             int v = e[1];
             int t = e[2];
+
             adj.get(u).add(new int[]{v, t});
             adj.get(v).add(new int[]{u, t});
         }
-        int[] time = new int[n];
-        Arrays.fill(time, Integer.MAX_VALUE);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[0] - b[0]);
-        pq.offer(new int[]{0, 0});//time, node
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
+        pq.offer(new int[]{0, 0});
 
-        time[0] = 0;
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
         while(!pq.isEmpty()){
             int[] a=pq.poll();
-            int currTim = a[0];
-            int curr=a[1];
-            if(currTim >= disappear[curr] || currTim > time[curr]){
+            int curr = a[0];
+            int currtime = a[1];
+            if(currtime > dist[curr] || currtime >= disappear[curr]){
                 continue;
             }
-            for(int[] node: adj.get(curr)){
-                int neigh = node[0];
-                int tm = node[1];
-                if(currTim + tm < disappear[neigh] && currTim + tm < time[neigh]){
-                    time[neigh] = currTim + tm;
-                    pq.offer(new int[]{time[neigh], neigh});
+            for(int[] arr: adj.get(curr)){
+                int neigh = arr[0];
+                int neightime = arr[1];
+                if(currtime + neightime < disappear[neigh] && currtime + neightime < dist[neigh]){
+                    dist[neigh] = currtime + neightime;
+                    pq.offer(new int[]{neigh, dist[neigh]});
                 }
             }
         }
         for(int i=0;i<n;i++){
-            if(time[i] == Integer.MAX_VALUE){
-                time[i] = -1;
+            if(dist[i] == Integer.MAX_VALUE){
+                dist[i] = -1;
             }
         }
-        return time;
+        return dist;
     }
 }
