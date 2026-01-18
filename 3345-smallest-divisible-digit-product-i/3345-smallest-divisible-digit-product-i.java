27deb85fce0f434504a930ed8f8f3b48c4810ab1
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-19 00:18:20 +0530

    Time: 1 ms (98.95%), Space: 42.5 MB (71.93%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int digProduct(int n) {
+        int pro = 1;
+        while(n > 0){
+            int r = n % 10;
+            pro *= r;
+            n /= 10;
+        }
+        return pro;
+    }
+    public int smallestNumber(int n, int t) {
+        for(int i=n;i<10000;i++){
+            if(digProduct(i) % t == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
