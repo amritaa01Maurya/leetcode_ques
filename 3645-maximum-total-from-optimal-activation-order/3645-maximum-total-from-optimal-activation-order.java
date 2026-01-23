37d39908ae239a88ce48864a96e6717edb40664a
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-23 22:35:16 +0530

    Time: 114 ms (44.37%), Space: 197.5 MB (19.87%) - LeetHub

--- 3645-maximum-total-from-optimal-activation-order/3645-maximum-total-from-optimal-activation-order.java
+++ 3645-maximum-total-from-optimal-activation-order/3645-maximum-total-from-optimal-activation-order.java
@@ -1,31 +1,20 @@
 class Solution {
     public long maxTotal(int[] value, int[] limit) {
         int n= value.length;
-        int[][] arr = new int[n][2];
+        
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
         for(int i=0;i<n;i++){
-            arr[i][0]= value[i];
-            arr[i][1]= limit[i];
+            map.computeIfAbsent(limit[i], k -> new PriorityQueue<>()).add(value[i]);
+            if(map.get(limit[i]).size() > limit[i]){
+                map.get(limit[i]).poll();
+            }
         }
 
-        Arrays.sort(arr, (a, b) ->{
-            if(a[1] != b[1]){
-                return Integer.compare(a[1], b[1]);
-            }
-            return Integer.compare(b[0], a[0]);
-        });
-
         long score = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int actEle = 0;
-        for(int i=0;i<n;i++){
-            if(actEle < arr[i][1]){//limit is  active element
-                pq.offer(arr[i][1]);
-                score += arr[i][0];
-                int x = pq.size(); // curr active element
-                actEle = Math.max(x, actEle);
-                while(!pq.isEmpty() && pq.peek() <= x){
-                    pq.poll();
-                }
+        for(int key:map.keySet()){
+            PriorityQueue<Integer> pq = map.get(key);
+            while(!pq.isEmpty()){
+                score += pq.poll();
             }
         }
         return score;
