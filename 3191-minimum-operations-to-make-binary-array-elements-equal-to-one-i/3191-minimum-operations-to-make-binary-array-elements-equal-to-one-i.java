b6d4e16cfc9f712c82eabfc12b5e18156434ad16
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-27 15:32:16 +0530

    Time: 18 ms (4.73%), Space: 103.3 MB (8.88%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,20 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
         int n = nums.length;
-        int flip = 0;
-        for(int i=0;i < n - 2;i++){
-            if(nums[i] == 0){// if ele is 0 flip it and next 2 consecutive ele;
-                nums[i] = 1;
-                nums[i+1] = nums[i+1] == 0 ? 1 : 0;
-                nums[i+2] = nums[i+2] == 0 ? 1 : 0;
-                flip++;
-            }
-        }
+        Queue<Integer> q = new LinkedList<>();
 
-        // check if all ele are one on not
-        for(int num:nums){
-            if(num != 1){
-                return -1;
+        int flip = 0;
+        for(int i=0;i < n;i++){
+            while(!q.isEmpty() && q.peek() + 2 < i ){// in q the flip is not affecting curr ele remove it
+                q.poll();
+            }
+            // (ele + no. of flips affecting it) 
+            if((nums[i] + q.size()) % 2 == 0){// if ele is 0(check if there is flip effecting it)
+                if(i + 2 >= n){ // if last 2 ele is zero then we can not apply 3 cons. flipping rule in it
+                    return -1;
+                }
+                flip++;
+                q.add(i);// here we flip the ele so add it
             }
         }
 
