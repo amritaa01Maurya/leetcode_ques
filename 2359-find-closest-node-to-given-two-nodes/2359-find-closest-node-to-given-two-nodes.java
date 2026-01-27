8a9d57652596e9327cae61db718b615d4c26577d
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-27 22:24:57 +0530

    Time: 70 ms (17.2%), Space: 170.8 MB (11.3%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,12 +1,12 @@
 class Solution {
-    public void solve(List<List<Integer>> adj, int src, int[] dist) {
+    public void bfs(List<List<Integer>> adj, int src, int[] dist) {
         Queue<Integer> q = new LinkedList<>();
         q.add(src);
-        
+
         while (!q.isEmpty()) {
-            int curr = q.remove();
+            int curr = q.poll();
             for (int neigh : adj.get(curr)) {
-                if (dist[curr] + 1 < dist[neigh] && dist[curr] != Integer.MAX_VALUE) {
+                if (dist[neigh] == -1) {
                     dist[neigh] = dist[curr] + 1;
                     q.add(neigh);
                 }
@@ -20,6 +20,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
+
         for (int i = 0; i < n; i++) {
             if (edges[i] != -1) {
                 adj.get(i).add(edges[i]);
@@ -27,26 +28,25 @@ class Solution {
         }
 
         int[] dist1 = new int[n];
-        Arrays.fill(dist1, Integer.MAX_VALUE);
-
-        dist1[node1] = 0;
-        solve(adj, node1, dist1);
-
         int[] dist2 = new int[n];
-        Arrays.fill(dist2, Integer.MAX_VALUE);
+        Arrays.fill(dist1, -1);
+        dist1[node1] = 0;
 
+        Arrays.fill(dist2, -1);
         dist2[node2] = 0;
-        solve(adj, node2, dist2);
 
-        int maxdist = Integer.MAX_VALUE;
+        // cal dist of each node from node1 and node2
+        bfs(adj, node1, dist1);
+        bfs(adj, node2, dist2);
+        
+        int minDist = Integer.MAX_VALUE;
         int node = -1;
         for (int i = 0; i < n; i++) {
-            if (dist1[i] != Integer.MAX_VALUE && dist2[i] != Integer.MAX_VALUE - 1
-                    && maxdist > Math.max(dist1[i], dist2[i])) {
-                maxdist = Math.max(dist1[i], dist2[i]);
+            if (dist1[i] != -1 && dist2[i] != -1 && minDist > Math.max(dist1[i], dist2[i])) {// we want node that can be reach from node1 and node2 both, and its dist minimized (such that max dist of node from node1 and node2 taken) 
+                minDist = Math.max(dist1[i], dist2[i]);
                 node = i;
             }
         }
-        return maxdist == Integer.MAX_VALUE ? -1 : node;
+        return minDist == Integer.MAX_VALUE ? -1 : node;
     }
 }
