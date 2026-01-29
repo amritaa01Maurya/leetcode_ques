42ed2f180304fa37524f1b03cd8ed50c8e64e50f
Amrita Maurya <mauryaamrita835@gmail.com>
2026-01-30 00:18:08 +0530

    Time: 40 ms (5.43%), Space: 101.6 MB (5.17%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -2,22 +2,30 @@ class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
         int n = nums.length;
         int idx = -1;// last index of ele out of range (mink, maxk)
-        int min = -1;
-        int max = -1;
+        Deque<Integer> min = new ArrayDeque<>();
+        Deque<Integer> max = new ArrayDeque<>();
 
         long count = 0;
         for(int i=0;i<n;i++){
             if(nums[i] < minK || nums[i] > maxK){
+                min.clear();
+                max.clear();
                 idx = i;
+                continue;
             }
-            if(nums[i] == minK ){
-                min = i;
+            while(!min.isEmpty() && nums[min.peekLast()] >= nums[i]){
+                min.pollLast();
             }
-            if(nums[i] == maxK){
-                max = i;
+            min.addLast(i);
+            while(!max.isEmpty() && nums[max.peekLast()] <= nums[i]){
+                max.pollLast();
             }
+            max.addLast(i);
             
-            count += Math.max(0, Math.min(min, max) - idx);//valid subarrays are b/w last invalid idx and min /max indices
+            if(nums[min.peekFirst()] == minK && nums[max.peekFirst()] == maxK){
+
+                count += Math.max(0, Math.min(min.peekFirst(), max.peekFirst()) - idx);//valid subarrays are b/w last invalid idx and min /max indices
+            }
         }
         return count;
     }
