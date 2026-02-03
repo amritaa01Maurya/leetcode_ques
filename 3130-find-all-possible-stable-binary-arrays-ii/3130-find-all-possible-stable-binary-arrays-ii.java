ca290c09b6e26d53041b5e7d784565e18177d82a
Amrita Maurya <mauryaamrita835@gmail.com>
2026-02-03 22:56:24 +0530

    Time: 212 ms (75%), Space: 232.3 MB (70.45%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.java
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.java
@@ -1,27 +1,31 @@
 class Solution {
     int mod = 1000000007;
-    public int numberOfStableArrays(int ze, int on, int lim) {
-        int[][][] dp = new int[ze+1][on+1][2];
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int[][][] dp = new int[zero+1][one+1][2];
         
-        for(int i=0;i<=Math.min(ze, lim);i++){
+        for(int i=0;i<= Math.min(zero, limit);i++){// up to limit sub array is valid containing only zero
             dp[i][0][0] = 1;
+
         }
-        for(int i=0;i<=Math.min(on, lim);i++){
+        for(int i=0;i<= Math.min(one, limit);i++){// up to limit sub array is valid containing only one
             dp[0][i][1] = 1;
         }
+        for(int z=1;z<=zero;z++){
+            for(int o=1;o<=one;o++){
+                // if count of zero is within limit than we can add zero to subarray ending with 0 or 1
+                dp[z][o][0] = (dp[z-1][o][1] + dp[z - 1][o][0])%mod;
 
-        for(int z=1;z<=ze;z++){
-            for(int o=1;o<=on;o++){
-                dp[z][o][0] = (dp[z-1][o][0] + dp[z-1][o][1])%mod;
-                dp[z][o][1] = (dp[z][o-1][0] + dp[z][o-1][1])%mod;
-                if(z > lim){
-                    dp[z][o][0] = (dp[z][o][0] - dp[z-lim- 1][o][1] + mod) % mod;
+                if(z > limit){// if count of zero exceed the limit we must substract subarrays
+                    dp[z][o][0] = (dp[z][o][0] - dp[z - limit - 1][o][1] + mod) % mod;
                 }
-                if(o > lim){
-                    dp[z][o][1] = (dp[z][o][1] - dp[z][o-lim- 1][0] + mod) % mod;
+
+                dp[z][o][1] = (dp[z][o - 1][1] + dp[z][o - 1][0])%mod;
+
+                if(o > limit){
+                    dp[z][o][1] = (dp[z][o][1] - dp[z][o - limit - 1][0] + mod) % mod;
                 }
-            } 
+            }
         }
-        return (dp[ze][on][0] + dp[ze][on][1])%mod;
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
     }
 }
