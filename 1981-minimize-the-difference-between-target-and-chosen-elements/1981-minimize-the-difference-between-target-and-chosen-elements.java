c3d967d8aec6916d75a5152a4077f98c01f3f27b
Amrita Maurya <mauryaamrita835@gmail.com>
2026-02-03 14:33:37 +0530

    Time: 767 ms (22.49%), Space: 50.8 MB (46.59%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.java
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int solve(int[][] mat, int target, int r, int sum, int[][] dp) {
+    public int solve(int[][] mat, int t, int r, int sum, int[][] dp) {
         int n  = mat.length;
+        int m  = mat[0].length;
         if(r > n){
             return Integer.MAX_VALUE;
         }
 
         if(r == n){
-            return Math.abs(sum - target);
+            return Math.abs(sum - t);
         }
 
         if(dp[r][sum] != -1){
             return dp[r][sum];
         }
+
         int ans = Integer.MAX_VALUE;
-        for(int c=0;c<mat[0].length;c++){
-            ans = Math.min(ans, solve(mat, target, r + 1, sum + mat[r][c], dp));
+        for(int c=0;c<m;c++){
+            ans = Math.min(ans, solve(mat, t, r + 1, sum + mat[r][c], dp));
         }
         
         return dp[r][sum] = ans;
