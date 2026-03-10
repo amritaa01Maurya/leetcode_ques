f6112bf1d3efa3674ecc5b71f8224bc0b2643828
Amrita Maurya <mauryaamrita835@gmail.com>
2026-03-10 11:15:31 +0530

    Time: 20 ms (82.99%), Space: 56.4 MB (74.99%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,15 +1,17 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int boat = 1;
-        int curr = 0;
-        for(int p:people){
-            if(curr + p <= limit){
-                curr += p;
-            }else{
-                curr = p;
-                boat++;
+        int boat = 0;
+        int s = 0;
+        int e = people.length - 1;
+        while(s <= e){
+            if(people[s] + people[e] <= limit){// if min & max wt people can fit under one boat
+                s++;
+                e--;
+            }else{// if max will not fit with min then it will take one boat space for itself
+                e--;
             }
+            boat++;
         }
         return boat;
     }
