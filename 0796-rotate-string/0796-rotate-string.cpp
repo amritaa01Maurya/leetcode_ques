fa9af7bb9c701b31cbd75097e4d9d7b8bf8f85e2
Amrita Maurya <mauryaamrita835@gmail.com>
2026-05-04 01:22:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (7.45%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()){
+            return false;
+        }
+
+        for(int i=0;i<s.size(); i++){
+            string st = s.substr(i) + s.substr(0, i);
+            if(st == goal){
+                return true;
+            }
+        }
+        return false;
+    }
+};
