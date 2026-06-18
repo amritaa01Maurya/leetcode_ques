9e1c27876599c1d3e484b184d33000fa59ae9de0
Amrita Maurya <mauryaamrita835@gmail.com>
2026-06-18 12:20:25 +0530

    Time: 0 ms (100%), Space: 8.1 MB (46.25%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -5,10 +5,10 @@ public:
         double min1 = 6; // 1 min -> 6 deg
         double aff = 0.5;// min hands affects hour hands by 0.5 deg extra
 
-        double ang1 = (hour % 12)* hr1 + (minutes * aff);
+        double ang1 = hour * hr1 + (minutes * aff);
         double ang2 = minutes * min1;
 
-        double ans = ang1 > ang2 ? ang1 - ang2 : ang2 - ang1;
-        return 360.0 - ans > ans ? ans: 360.0 - ans; 
+        double ans = abs(ang2 - ang1);
+        return min(ans, 360 - ans); 
     }
 };
